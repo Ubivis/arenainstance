316b342cc45ab0d17c6204c84fd9563ec527a72f
Ubivis <gunnar.steincke@hamburg.de>
2025-02-25 10:01:00 +0100

    Update Arena.java

--- src/main/java/com/ubivismedia/arenaplugin/arena/Arena.java
+++ src/main/java/com/ubivismedia/arenaplugin/arena/Arena.java
@@ -1,1 +1,23 @@
+package com.ubivismedia.arenaplugin.arena;
 
+public class Arena {
+    private final String name;
+    private boolean active;
+
+    public Arena(String name) {
+        this.name = name;
+        this.active = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
