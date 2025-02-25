8dce5a63aecfac74426ebd67404813dafacfd2d0
Ubivis <gunnar.steincke@hamburg.de>
2025-02-25 09:58:30 +0100

    Update ArenaInstance.java

--- src/main/java/com/ubivismedia/arenaplugin/ArenaInstance.java
+++ src/main/java/com/ubivismedia/arenaplugin/ArenaInstance.java
@@ -1,1 +1,19 @@
+package com.ubivismedia.arenaplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ArenaInstance extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Arena Plugin wurde aktiviert!");
+        // Initialisierung der Befehle und Event-Listener
+        getCommand("arena").setExecutor(new ArenaCommand(this));
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Arena Plugin wurde deaktiviert!");
+    }
+}
 
