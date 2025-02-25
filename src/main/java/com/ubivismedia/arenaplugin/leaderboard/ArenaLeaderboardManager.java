0aed8ce2b1905a0dc4afc443427fd0d5378b09e5
Ubivis <gunnar.steincke@hamburg.de>
2025-02-25 13:39:10 +0100

    Update ArenaLeaderboardManager.java

--- src/main/java/com/ubivismedia/arenaplugin/leaderboard/ArenaLeaderboardManager.java
+++ src/main/java/com/ubivismedia/arenaplugin/leaderboard/ArenaLeaderboardManager.java
@@ -2,12 +2,15 @@ package com.ubivismedia.arenaplugin.leaderboard;
 
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
+import org.bukkit.Location;
 import java.util.*;
 
 public class ArenaLeaderboardManager {
     
     private final Map<UUID, Integer> playerScores = new HashMap<>();
+    private Location leaderboardLocation;
     
     public void addScore(Player player, int score) {
         playerScores.put(player.getUniqueId(), getScore(player) + score);
@@ -20,6 +23,7 @@ public class ArenaLeaderboardManager {
     public void resetScores() {
         playerScores.clear();
         Bukkit.broadcastMessage("Alle Arena-Scores wurden zurückgesetzt!");
+        updateLeaderboardDisplay();
     }
     
     public List<Map.Entry<UUID, Integer>> getTopPlayers(int limit) {
@@ -37,5 +41,25 @@ public class ArenaLeaderboardManager {
             player.sendMessage("§e" + rank + ". " + offlinePlayer.getName() + " - " + entry.getValue() + " Punkte");
             rank++;
         }
+    }
+    
+    public void setLeaderboardLocation(Location location) {
+        this.leaderboardLocation = location;
+    }
+    
+    public void updateLeaderboardDisplay() {
+        if (leaderboardLocation == null || !(leaderboardLocation.getBlock().getState() instanceof Sign)) {
+            return;
+        }
+        
+        Sign sign = (Sign) leaderboardLocation.getBlock().getState();
+        List<Map.Entry<UUID, Integer>> topPlayers = getTopPlayers(4);
+        
+        sign.setLine(0, "§6Arena Highscores");
+        for (int i = 0; i < topPlayers.size(); i++) {
+            OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(topPlayers.get(i).getKey());
+            sign.setLine(i + 1, "§e" + (i + 1) + ". " + offlinePlayer.getName());
+        }
+        sign.update();
     }
 }
