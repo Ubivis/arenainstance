4fa7951b17a3704ebb2680c450fb0cc8b1c158cf
Ubivis <gunnar.steincke@hamburg.de>
2025-02-25 10:03:03 +0100

    Update ArenaInstance.java

--- src/main/java/com/ubivismedia/arenaplugin/ArenaInstance.java
+++ src/main/java/com/ubivismedia/arenaplugin/ArenaInstance.java
@@ -3,10 +3,12 @@ package com.ubivismedia.arenaplugin;
 import com.ubivismedia.arenaplugin.arena.ArenaManager;
 import com.ubivismedia.arenaplugin.commands.ArenaCommand;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.io.*;
 
 public class ArenaInstance extends JavaPlugin {
 
     private ArenaManager arenaManager;
+    private final File arenasFile = new File(getDataFolder(), "arenas.dat");
 
     @Override
     public void onEnable() {
@@ -14,6 +16,7 @@ public class ArenaInstance extends JavaPlugin {
         
         // Initialisierung des ArenaManagers
         arenaManager = new ArenaManager();
+        loadArenas();
         
         // Initialisierung der Befehle
         getCommand("arena").setExecutor(new ArenaCommand(arenaManager));
@@ -22,5 +25,25 @@ public class ArenaInstance extends JavaPlugin {
     @Override
     public void onDisable() {
         getLogger().info("Arena Plugin wurde deaktiviert!");
+        saveArenas();
+    }
+
+    private void saveArenas() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(arenasFile))) {
+            oos.writeObject(arenaManager.getArenas());
+            getLogger().info("Arenen gespeichert.");
+        } catch (IOException e) {
+            getLogger().severe("Fehler beim Speichern der Arenen: " + e.getMessage());
+        }
+    }
+
+    private void loadArenas() {
+        if (!arenasFile.exists()) return;
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(arenasFile))) {
+            arenaManager.loadArenas((Map<String, Arena>) ois.readObject());
+            getLogger().info("Arenen geladen.");
+        } catch (IOException | ClassNotFoundException e) {
+            getLogger().severe("Fehler beim Laden der Arenen: " + e.getMessage());
+        }
     }
 }
