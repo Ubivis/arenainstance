e654fbb424778750605c237c84cc9a5d2d5fc136
Ubivis <gunnar.steincke@hamburg.de>
2025-02-25 11:07:59 +0100

    Update ArenaCommand.java

--- src/main/java/com/ubivismedia/arenaplugin/commands/ArenaCommand.java
+++ src/main/java/com/ubivismedia/arenaplugin/commands/ArenaCommand.java
@@ -1,120 +1,77 @@
-package com.ubivismedia.arenaplugin.commands;
+package com.ubivismedia.arenaplugin.arena;
 
-import com.ubivismedia.arenaplugin.arena.ArenaManager;
-import org.bukkit.GameMode;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import org.bukkit.Location;
-import org.bukkit.Bukkit;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.Material;
-import org.bukkit.World;
-import org.bukkit.WorldCreator;
-import org.bukkit.inventory.PlayerInventory;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.entity.EntityType;
 
-public class ArenaCommand implements CommandExecutor {
+public class ArenaManager {
     
-    private final ArenaManager arenaManager;
-    private final Map<Player, Location> playerLocations = new HashMap<>();
-    private final Map<Player, ItemStack[]> savedInventories = new HashMap<>();
-    private final Map<Player, ItemStack[]> specialBlocks = new HashMap<>();
+    private final Map<String, Arena> arenas = new HashMap<>();
+    private final File arenaFile;
+    private final FileConfiguration arenaConfig;
     
-    public ArenaCommand(ArenaManager arenaManager) {
-        this.arenaManager = arenaManager;
+    public ArenaManager() {
+        arenaFile = new File("plugins/ArenaInstance/arenas.yml");
+        if (!arenaFile.exists()) {
+            try {
+                arenaFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        arenaConfig = YamlConfiguration.loadConfiguration(arenaFile);
+        loadArenas();
     }
     
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage("Nur Spieler können diesen Befehl ausführen!");
-            return true;
+    public void createArena(String name) {
+        if (arenas.containsKey(name)) {
+            throw new IllegalArgumentException("Eine Arena mit diesem Namen existiert bereits!");
         }
-        
-        Player player = (Player) sender;
-        
-        if (args.length < 2 && !args[0].equalsIgnoreCase("leave") && !args[0].equalsIgnoreCase("edit")) {
-            player.sendMessage("Benutzung: /arena <create|delete|join|leave|start|edit> <ArenaName>");
-            return true;
+        Arena arena = new Arena(name);
+        arenas.put(name, arena);
+        saveArenas();
+    }
+    
+    public void removeArena(String name) {
+        if (arenas.containsKey(name)) {
+            arenas.remove(name);
+            arenaConfig.set("arenas." + name, null);
+            saveArenas();
         }
-        
-        String action = args[0].toLowerCase();
-        String arenaName = args.length > 1 ? args[1] : "";
-
-        switch (action) {
-            case "create":
-                if (arenaManager.arenaExists(arenaName)) {
-                    player.sendMessage("Eine Arena mit diesem Namen existiert bereits!");
-                    return true;
-                }
-                arenaManager.createArena(arenaName);
-                player.sendMessage("Arena " + arenaName + " wurde erfolgreich erstellt!");
-                break;
-            
-            case "delete":
-                if (!arenaManager.arenaExists(arenaName)) {
-                    player.sendMessage("Diese Arena existiert nicht!");
-                    return true;
-                }
-                arenaManager.removeArena(arenaName);
-                player.sendMessage("Arena " + arenaName + " wurde gelöscht!");
-                break;
-                
-            case "join":
-                if (!arenaManager.arenaExists(arenaName)) {
-                    player.sendMessage("Diese Arena existiert nicht!");
-                    return true;
-                }
-                playerLocations.put(player, player.getLocation());
-                player.teleport(arenaManager.getArena(arenaName).getSpawnLocation());
-                player.sendMessage("Du bist der Arena " + arenaName + " beigetreten!");
-                break;
-            
-            case "leave":
-                if (!playerLocations.containsKey(player)) {
-                    player.sendMessage("Du bist derzeit in keiner Arena!");
-                    return true;
-                }
-                player.teleport(playerLocations.remove(player));
-                if (savedInventories.containsKey(player)) {
-                    player.getInventory().setContents(savedInventories.remove(player));
-                }
-                player.sendMessage("Du hast die Arena verlassen!");
-                break;
-                
-            case "edit":
-                if (!arenaManager.arenaExists(arenaName)) {
-                    player.sendMessage("Diese Arena existiert nicht!");
-                    return true;
-                }
-                playerLocations.put(player, player.getLocation());
-                savedInventories.put(player, player.getInventory().getContents());
-                player.teleport(arenaManager.getArena(arenaName).getSpawnLocation());
-                player.setGameMode(GameMode.CREATIVE);
-                
-                ItemStack[] specialItems = new ItemStack[]{
-                    new ItemStack(Material.GOLD_BLOCK, 1), // Beispiel für Startblock
-                    new ItemStack(Material.EMERALD_BLOCK, 1), // Beispiel für Wellenblock
-                    new ItemStack(Material.CHEST, 1), // Beispiel für Waffenkiste
-                    new ItemStack(Material.DIAMOND_BLOCK, 1) // Beispiel für Zuschauerblock
-                };
-                
-                specialBlocks.put(player, specialItems);
-                player.getInventory().setContents(specialItems);
-                
-                player.sendMessage("Du bearbeitest nun die Arena " + arenaName + "! Spezielle Blöcke wurden deinem Inventar hinzugefügt.");
-                break;
-                
-            default:
-                player.sendMessage("Unbekannter Arena-Befehl! Nutze /arena <create|delete|join|leave|start|edit> <ArenaName>");
-                break;
+    }
+    
+    public boolean arenaExists(String name) {
+        return arenas.containsKey(name);
+    }
+    
+    public Arena getArena(String name) {
+        return arenas.get(name);
+    }
+    
+    public void saveArenas() {
+        for (String name : arenas.keySet()) {
+            arenaConfig.set("arenas." + name + ".world", name);
         }
-        
-        return true;
+        try {
+            arenaConfig.save(arenaFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void loadArenas() {
+        if (arenaConfig.contains("arenas")) {
+            for (String name : arenaConfig.getConfigurationSection("arenas").getKeys(false)) {
+                World world = Bukkit.getWorld(name);
+                if (world != null) {
+                    arenas.put(name, new Arena(name));
+                }
+            }
+        }
     }
 }
