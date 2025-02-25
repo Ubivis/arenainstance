39ee1869053fa3e2ef98fee03921a5045774fa55
Ubivis <gunnar.steincke@hamburg.de>
2025-02-25 11:55:54 +0100

    Update ArenaSpectatorManager.java

--- src/main/java/com/ubivismedia/arenaplugin/arena/ArenaSpectatorManager.java
+++ src/main/java/com/ubivismedia/arenaplugin/arena/ArenaSpectatorManager.java
@@ -11,14 +11,18 @@ import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import org.bukkit.scheduler.BukkitRunnable;
+import java.util.*;
 
 public class ArenaSpectatorManager implements CommandExecutor {
     
     private final Map<UUID, String> spectators = new HashMap<>();
     private final Map<UUID, ArmorStand> spectatorAvatars = new HashMap<>();
+    private final Random random = new Random();
+    
+    public ArenaSpectatorManager() {
+        startSpectatorAnimations();
+    }
     
     public void addSpectator(Player player, String arenaName, Location spectatorBlockLocation) {
         spectators.put(player.getUniqueId(), arenaName);
@@ -63,6 +67,32 @@ public class ArenaSpectatorManager implements CommandExecutor {
     
     public boolean isSpectating(Player player) {
         return spectators.containsKey(player.getUniqueId());
+    }
+    
+    private void startSpectatorAnimations() {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (ArmorStand stand : spectatorAvatars.values()) {
+                    int action = random.nextInt(3);
+                    switch (action) {
+                        case 0: // Kopf leicht drehen
+                            stand.setHeadPose(stand.getHeadPose().setY(stand.getHeadPose().getY() + 0.1));
+                            break;
+                        case 1: // Jubeln (kurz nach oben bewegen)
+                            stand.teleport(stand.getLocation().add(0, 0.2, 0));
+                            Bukkit.getScheduler().runTaskLater(Bukkit.getPluginManager().getPlugin("ArenaInstance"), () -> 
+                                stand.teleport(stand.getLocation().add(0, -0.2, 0)), 10L);
+                            break;
+                        case 2: // Winken (Arme hoch und runter bewegen)
+                            stand.setArms(true);
+                            Bukkit.getScheduler().runTaskLater(Bukkit.getPluginManager().getPlugin("ArenaInstance"), () ->
+                                stand.setArms(false), 10L);
+                            break;
+                    }
+                }
+            }
+        }.runTaskTimer(Bukkit.getPluginManager().getPlugin("ArenaInstance"), 0L, 100L);
     }
     
     @Override
