c8f0dda5b396930da7a87324b036b13f0708d6f2
Ubivis <gunnar.steincke@hamburg.de>
2025-02-25 10:22:26 +0100

    Update ArenaCommand.java

--- src/main/java/com/ubivismedia/arenaplugin/commands/ArenaCommand.java
+++ src/main/java/com/ubivismedia/arenaplugin/commands/ArenaCommand.java
@@ -5,10 +5,14 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import java.util.HashMap;
+import java.util.Map;
+import org.bukkit.Location;
 
 public class ArenaCommand implements CommandExecutor {
     
     private final ArenaManager arenaManager;
+    private final Map<Player, Location> playerLocations = new HashMap<>();
     
     public ArenaCommand(ArenaManager arenaManager) {
         this.arenaManager = arenaManager;
@@ -23,13 +27,13 @@ public class ArenaCommand implements CommandExecutor {
         
         Player player = (Player) sender;
         
-        if (args.length < 2) {
-            player.sendMessage("Benutzung: /arena <create|delete> <ArenaName>");
+        if (args.length < 2 && !args[0].equalsIgnoreCase("leave")) {
+            player.sendMessage("Benutzung: /arena <create|delete|join|leave> <ArenaName>");
             return true;
         }
         
         String action = args[0].toLowerCase();
-        String arenaName = args[1];
+        String arenaName = args.length > 1 ? args[1] : "";
 
         switch (action) {
             case "create":
@@ -49,9 +53,28 @@ public class ArenaCommand implements CommandExecutor {
                 arenaManager.removeArena(arenaName);
                 player.sendMessage("Arena " + arenaName + " wurde gelöscht!");
                 break;
+                
+            case "join":
+                if (!arenaManager.arenaExists(arenaName)) {
+                    player.sendMessage("Diese Arena existiert nicht!");
+                    return true;
+                }
+                playerLocations.put(player, player.getLocation());
+                player.teleport(arenaManager.getArena(arenaName).getSpawnLocation());
+                player.sendMessage("Du bist der Arena " + arenaName + " beigetreten!");
+                break;
             
+            case "leave":
+                if (!playerLocations.containsKey(player)) {
+                    player.sendMessage("Du bist derzeit in keiner Arena!");
+                    return true;
+                }
+                player.teleport(playerLocations.remove(player));
+                player.sendMessage("Du hast die Arena verlassen!");
+                break;
+                
             default:
-                player.sendMessage("Unbekannter Arena-Befehl! Nutze /arena <create|delete> <ArenaName>");
+                player.sendMessage("Unbekannter Arena-Befehl! Nutze /arena <create|delete|join|leave> <ArenaName>");
                 break;
         }
         
