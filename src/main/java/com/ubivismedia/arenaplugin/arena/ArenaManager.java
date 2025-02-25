478d414016d6d735b4d1d16b0d2e146e2649402b
Ubivis <gunnar.steincke@hamburg.de>
2025-02-25 10:00:24 +0100

    Update ArenaManager.java

--- src/main/java/com/ubivismedia/arenaplugin/arena/ArenaManager.java
+++ src/main/java/com/ubivismedia/arenaplugin/arena/ArenaManager.java
@@ -1,1 +1,33 @@
+package com.ubivismedia.arenaplugin.arena;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArenaManager {
+
+    private final Map<String, Arena> arenas = new HashMap<>();
+
+    public void createArena(String name) {
+        if (arenas.containsKey(name)) {
+            throw new IllegalArgumentException("Eine Arena mit diesem Namen existiert bereits!");
+        }
+        Arena arena = new Arena(name);
+        arenas.put(name, arena);
+    }
+
+    public Arena getArena(String name) {
+        return arenas.get(name);
+    }
+
+    public void removeArena(String name) {
+        arenas.remove(name);
+    }
+
+    public boolean arenaExists(String name) {
+        return arenas.containsKey(name);
+    }
+
+    public Map<String, Arena> getArenas() {
+        return arenas;
+    }
+}
